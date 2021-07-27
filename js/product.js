9d5c75bbda9a648dc61b1dffdef4997b51609682
monion95 <38639195+monion95@users.noreply.github.com>
2021-07-27 10:14:26 -0400

    Update product.js

--- js/product.js
+++ js/product.js
@@ -1,7 +1,8 @@
 function generateProductPage(product){
+    //get product name from api
     document.title = product.name;
 
-
+    //generating web page
     return`
     <section class="product">
         <div class="product-texts">
@@ -35,16 +36,21 @@ function generateProductPage(product){
 
 
 
-
+// eventlistenner, load functions when page is loaded
 window.addEventListener('load', async (event) => {
+
+    //declarations
     const params = getQueryParameters();
     const product = await getProductById(params.id);
     const main = document.getElementsByTagName('main')[0];
+
+    //call generateProductPage to generate dynamic web content
     main.innerHTML = generateProductPage(product);
 
     //create eventlistener for quantity
     document.getElementById("quantity").addEventListener("change", updateTotalPrice);
 
+    //update price when change quantity
     function updateTotalPrice(){
         var quantity  = document.getElementById("quantity").value;
         document.getElementById("product-price").textContent = "$"+ (quantity * product.price).toFixed(2);
