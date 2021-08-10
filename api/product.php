9be953d125252539fb1e367bae6953c37732ea89
monion95 <38639195+monion95@users.noreply.github.com>
2021-08-10 14:36:13 -0400

    Update product.php

--- api/product.php
+++ api/product.php
@@ -2,23 +2,31 @@
 
 require 'private/mysqli.php';
 
+/* get product from database by prepared sql*/
 function findProductById($id) {
   global $mysqli;
+  
+  // sql template
   $stmt = $mysqli->prepare("SELECT * FROM Product WHERE id = ?");
+
+  //bind id with string
   $stmt->bind_param("s", $id);
+
+  //execute the prepared sql statement and return result
   $stmt->execute();
   $result = $stmt->get_result();
   return $result->fetch_assoc();
 }
 
-switch ($_SERVER["REQUEST_METHOD"]) {
-  case 'GET':
-    $product = findProductById($_GET['id'] ?? '');
-    echo json_encode($product);
-    break;
-  default:
-    http_response_code(400);
-    break;
+
+/* response to method, including GET method or 
+ * default case gives error page with code 400 */
+
+if ($_SERVER["REQUEST_METHOD"]==='GET'){
+  $product = findProductById($_GET['id'] ?? '');
+  echo json_encode($product);
+}else{
+  http_response_code(400);
 }
 
 ?>
